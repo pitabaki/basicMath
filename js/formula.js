12175209edd80504ccad4ef607f1e925c8884a0e
Peter Berki <pberki1986@gmail.com>
2015-10-05 22:57:12 -0700

    Loop returns first number of an array

--- js/formula.js
+++ js/formula.js
@@ -16,6 +16,12 @@ function average(){
 	var numberArray = document.getElementById("num_array").value;
 	var numberString = [];
 	var numbers = [];
+	var transfer = "";
+	//*************** DELETE WHEN PROBLEM SOLVED *******************
+	var testPrint = document.getElementById("test");
+
+	//*************** DELETE WHEN PROBLEM SOLVED *******************
+
 	/*var numLength = document.getElementById("num_length").value;
 	numLength = Number(numLength);
 	//console.log(numberArray.length);*/
@@ -41,23 +47,54 @@ function average(){
 	console.log(numberString);
 	//declare a variable for calculating the sum
 	var something = "";
-
+	var counter = 0;
 	//for loop to cycle through the "numbers" array
 	for(var i = 0; i < numberString.length; i++){
 		//for each passing of the for loop, add the current value to the total of "something"
 		something += numberString[i];
 	}
+	if(testPrint.textContent !== "undefined"){
+		testPrint.textContent = something;
+	}else{
+		testPrint.innerText = something;
+	}
+
+	//Do/While/For that cycles through something and parses out the numbers
+	//For loop adds 1 for every encounter with a number
+	//Once it comes across a comma, counter's total is used as an in
+
+	//!!!!!!!!!!!!!! Fix This Loop; Only Cycling Through For Initial Number
+	do{
+		for(var h = 0; h < something.length; h++){
+			if(something[h] !== ","){
+				counter++;
+		        transfer += something[h];
+			}else{
+		        something = something.slice(counter + 1);
+		        transfer = Number(transfer);
+		        numbers.push(transfer);
+		        numbers.splice(1,1);
+		        console.log("numbers:" + numbers);
+		        transfer = "";
+		        counter = 0;
+		      console.log("something: " + something);
+		    }
+		}
+	}while(something.length > 0);
+	console.log("counter:" + counter);
 	console.log("something: " + something);
 	//parse out strings as integers
 	//cycle through the numbers array
-	for(var h = 0; h < something.length; h++){
+
+	//*************** DELETE WHEN PROBLEM SOLVED *******************
+	/*for(var h = 0; h < something.length; h++){
 		if(something[h] !== ","){
 			numbers.push(something[h]);
 		}else if(something[h] === ","){
 			numbers = Number(numbers);
 		}
-	}
-	console.log("this is " + numbers);
+	}*/
+	console.log("numbers: " + typeof numbers);
 	console.log(something /= numbers.length);
 	//Divide total of something by the length of numbers
 	return false;
