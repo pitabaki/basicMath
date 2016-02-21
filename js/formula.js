1d21f8fa25a55642a9ac3fe3abca89f9ca86d33b
Peter Berki <pberki1986@gmail.com>
2016-02-20 17:04:56 -0800

    Math working for average. User can enter any character and average will be calculated

--- js/formula.js
+++ js/formula.js
@@ -13,95 +13,51 @@ List of formulas:
 
 function average(){
 	'use strict';
-	var numberArray = document.getElementById("num_array").value;
-	var numberString = [];
-	var numbers = [];
-	var transfer = "";
-	//*************** DELETE WHEN PROBLEM SOLVED *******************
-	var testPrint = document.getElementById("test");
+	var numberArray = document.getElementById("num_array").value,
+		print = document.getElementById("print"),
+		number,
+    	count,
+    	total = 0,
+    	tempStorage = [],
+    	secondTemp = [],
+    	pushStorage = [];
 
-	//*************** DELETE WHEN PROBLEM SOLVED *******************
+	console.log(numberArray);
 
-	/*var numLength = document.getElementById("num_length").value;
-	numLength = Number(numLength);
-	//console.log(numberArray.length);*/
-	//console.log(typeof numberArray[7]);
-	/*for(var h = 0; h < numberArray.length; h++){
-		if((numberArray[h] !== ",") && (numberArray[h] !== " ")){
-			numLength++;
-		}else if((numberArray[h] === ",") || (numberArray[h] === " ")){
-			numLength = 0;
+	//convert numberArray from string to array
+	for(var y = 0; y < numberArray.length; y++){
+		//for all numbers, push to empty array
+		if(isNaN(parseFloat(numberArray[y])) !== true){
+			tempStorage.push(numberArray[y]);
+		//for characters that aren't numbers, but also aren't decimals
+		}else if((numberArray[y] === ',') || (numberArray[y] === ' ')){
+			pushStorage = tempStorage.join('');
+			secondTemp.push(pushStorage);
+			tempStorage = [];
+			pushStorage = [];
+		}else if(numberArray[y] === '.'){
+			tempStorage.push(numberArray[y]);
 		}
 	}
-	console.log(numLength);*/
-	for(var g = 0; g < numberArray.length; g++){
-		if((numberArray[g] !== ",") && (numberArray[g] !== " ")){
-			numberString.push(numberArray[g]);
-		}else if(numberArray[g] === " "){
-			numberString.push();
-		}else{
-			numberString.push(",");
-		}
-	}
-	//numberArray = Number(numberArray);
-	console.log(numberString);
-	//declare a variable for calculating the sum
-	var something = "";
-	var counter = 0;
-	//for loop to cycle through the "numbers" array
-	for(var i = 0; i < numberString.length; i++){
-		//for each passing of the for loop, add the current value to the total of "something"
-		something += numberString[i];
-	}
-	if(testPrint.textContent !== "undefined"){
-		testPrint.textContent = something;
-	}else{
-		testPrint.innerText = something;
+
+	if(tempStorage.length !== 0){
+		pushStorage = tempStorage.join('');
+		secondTemp.push(pushStorage);
+		pushStorage = [];
 	}
 
-	//Do/While/For that cycles through something and parses out the numbers
-	//For loop adds 1 for every encounter with a number
-	//Once it comes across a comma, counter's total is used as an in
+	count = secondTemp.length;
 
-	//!!!!!!!!!!!!!! Fix This Loop; Only Cycling Through For Initial Number
-	do{
-		for(var h = 0; h < something.length; h++){
-			if(something[h] !== ","){
-				counter++;
-		        transfer += something[h];
-			}else{
-		        something = something.slice(counter + 1);
-		        transfer = Number(transfer);
-		        numbers.push(transfer);
-		        numbers.splice(1,1);
-		        console.log("numbers:" + numbers);
-		        transfer = "";
-		        counter = 0;
-		      console.log("something: " + something);
-		    }
-		}
-	}while(something.length > 0);
-	console.log("counter:" + counter);
-	console.log("something: " + something);
-	//parse out strings as integers
-	//cycle through the numbers array
+	for(var g = 0; g < secondTemp.length; g++){
+		number = parseFloat(secondTemp[g], 2);
+		total += number;
+	}
+	//Divide total by the length count
+	total = total / count;
+	total = total.toFixed(2);
 
-	//*************** DELETE WHEN PROBLEM SOLVED *******************
-	/*for(var h = 0; h < something.length; h++){
-		if(something[h] !== ","){
-			numbers.push(something[h]);
-		}else if(something[h] === ","){
-			numbers = Number(numbers);
-		}
-	}*/
-	console.log("numbers: " + typeof numbers);
-	console.log(something /= numbers.length);
-	//Divide total of something by the length of numbers
 	return false;
 }
-
-//testing Average
-//console.log(average([39,42]));
 
 function init(){
 	'use strict';
